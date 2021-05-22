66bd93f8a85f0cffb3d8a2f55cc53ecf259ed561
Kp <kp_1@outlook.com>
2021-05-22 18:14:16 +0400

    Updates to API Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,74 +1,73 @@
 const router = require('express').Router();
-const Workout = require('../models');
+const Workout = require('../models/workout.js');
 const path = require('path');
-
-//Info for the workout page
-router.get('/api/workouts', (req, res) => {
-  Workout.Workout.find({})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+require('mongoose');
 
 //Create new workouts
 router.post('/api/workouts', (req, res) => {
-  Workout.Workout.create({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-//Get the last workout info for range
-router.get('/api/workouts/range', (req, res) => {
-  Workout.Workout.aggregate([
-    {
-      $addFields: {
-        totalDuration: {
-          $sum: '$exercises.duration',
-        },
-      },
-    },
-  ])
+  Workout.create({})
     .then((data) => {
       res.json(data);
     })
     .catch((err) => {
+      console.log('error', err);
       res.status(400).json(err);
     });
 });
 
-router.get('/api/workouts', (req, res) => {
-  Workout.Workout.aggregate([
-    {
-      $addFields: {
-        totalDuration: {
-          $sum: '$exercises.duration',
-        },
-      },
-    },
-  ])
-    .then((data) => {
-      res.json(data);
-    })
+//Add an exercise
+router.put('/api/workouts/:id', (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+
+    { $push: { exercises: req.body } },
+    { new: true, runValidators: true }
+  )
+
+    .then((dbWorkout) => res.json(data))
     .catch((err) => {
+      console.log('error', err);
+
       res.json(err);
     });
 });
 
-//Update workouts by id
-router.put('/api/workouts/:id', (req, res) => {
-  // const id = req.params.id;
-  // const body = req.body;
+//Get the last workout info for range - Ask Inst
+router.get('/api/workouts/range', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
-  Workout.Workout.findByIdAndUpdate(req.params.id, {
-    $push: { exercises: req.body },
-  })
+// Find all workouts within range
+
+router.get('/api/workouts/range', (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((data) => res.json(data))
+
+    .catch((err) => {
+      console.log('error', err);
+
+      res.json(err);
+    });
+});
+
+//Info for the last workout
+router.get('/api/workouts', (req, res) => {
+  Workout.find({})
     .then((data) => {
       res.json(data);
     })
